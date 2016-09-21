58ae3ac5acad6bdd9d5fb3a4c213c6acfdafbb2d
Liu Cheng <lc504906985@gmail.com>
2016-09-21 22:39:44 +0800

    add notify

--- server/config.js
+++ server/config.js
@@ -3,4 +3,5 @@
 exports.token = "YOUR_TOKEN";
 exports.appID = "YOUR_APPID";
 exports.appsecret = "YOUR_APPSECRET";
-exports.url = "YOUR_DOMAIN_NAME"
+exports.url = "YOUR_DOMAIN_NAME";
+exports.notify_templet_id = "YOUR_TEMPLET_ID";
