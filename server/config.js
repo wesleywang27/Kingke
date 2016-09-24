f55a22e46f05d8366ffe807050dac9bb142238dc
wangdeyi427 <744731059@qq.com>
2016-09-24 15:07:46 +0800

    update config.js

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@
 exports.token = "passw0rd";
 exports.appID = "wxe5bbb51184f14bbb";
 exports.appsecret = "5eac6742046a02ccf0dfe330c58c5bd4";
-exports.url = "http://www.wangdeyi.cc/kingke";
+exports.url = "http://www.wangdeyi.cc";
 exports.notify_templet_id = "YOUR_TEMPLET_ID";
