d1bb2eda972452c61d70fc608b32d9810d93e8b7
eric <744731059@qq.com>
2016-10-28 23:56:07 +0800

    修改分组

--- server/main.js
+++ server/main.js
@@ -34,8 +34,8 @@ Meteor.startup(() => {
       var token_url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + config.appID + "&secret=" + config.appsecret;
       var token_result = HTTP.get(token_url);
       var access_token = token_result.data.access_token;
-	  var group_url = "https://api.weixin.qq.com/cgi-bin/groups/delete?access_token=" + access_token;
-	  var group_data = '{"group":{"id":104}}';
+	  var group_url = "https://api.weixin.qq.com/cgi-bin/groups/create?access_token=" + access_token;
+	  var group_data = '{"group":{"name":"超级用户"}}';
 	  var group_result = HTTP.post(group_url,{content: group_data});
 	  res.end("set_success" + group_result.content);
 	}catch (err){
@@ -51,7 +51,7 @@ Meteor.startup(() => {
       var token_result = HTTP.get(token_url);
       var access_token = token_result.data.access_token;
       var group_url = "https://api.weixin.qq.com/cgi-bin/groups/members/update?access_token=" + access_token;
-      var group_data = '{"openid":"o0XCywkF-5S1fWSlQ4S2qctr9wt4","to_groupid":101}';
+      var group_data = '{"openid":"o0XCywvaCR--L3LBx5c-43X2VQvY","to_groupid":100}';
       var group_result = HTTP.post(group_url,{content: group_data});
       res.end("set_success" + group_result.content);
     }catch (err){
