393eb95b9f8863b7d874379f9c038e03897f0080
eric <744731059@qq.com>
2016-11-03 11:26:15 +0800

    修改config.js

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@ exports.appID = "wxe5bbb51184f14bbb";
 exports.appsecret = "5eac6742046a02ccf0dfe330c58c5bd4";
 exports.url = "www.wangdeyi.cc";
 exports.notify_template_id = 'Sovf0qxn7YDHR4OSDLxM6TagJRxN3dEj5zHSB9rGiGY';
-exports.follow_template_id = '	YcHO8iDkVOfHA-XulkP8yb5o1ohPhy_9-r4PBZjupGo';
+exports.follow_template_id = 'YcHO8iDkVOfHA-XulkP8yb5o1ohPhy_9-r4PBZjupGo';
 exports.register_template_id = '0-DB9x3A69-DADqrQip4q8FW73-2r3qmQ0cR66BOyQQ';
