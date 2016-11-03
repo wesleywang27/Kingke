de880faeb2c35f4a349419b7eb7f9471e0c35572
eric <744731059@qq.com>
2016-11-03 11:03:32 +0800

    修改config.js

--- server/config.js
+++ server/config.js
@@ -1,8 +1,9 @@
 // Warning!!!
 // Do Not Commit this file!!!
-exports.token = 'YOUR_TOKEN';
-exports.appID = 'YOUR_APPID';
-exports.appsecret = 'YOUR_APPSECRET';
-exports.url = 'YOUR_DOMAIN_NAME';
-exports.notify_template_id = 'YOUR_TEMPLET_ID';
-exports.follow_template_id = 'YOUR_TEMPLET_ID';
+exports.token = "passw0rd";
+exports.appID = "wxe5bbb51184f14bbb";
+exports.appsecret = "5eac6742046a02ccf0dfe330c58c5bd4";
+exports.url = "www.wangdeyi.cc";
+exports.notify_template_id = 'Sovf0qxn7YDHR4OSDLxM6TagJRxN3dEj5zHSB9rGiGY';
+exports.follow_template_id = '	YcHO8iDkVOfHA-XulkP8yb5o1ohPhy_9-r4PBZjupGo';
+exports.register_template_id = '0-DB9x3A69-DADqrQip4q8FW73-2r3qmQ0cR66BOyQQ';
