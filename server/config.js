0e924690a2b659f58916bccb5fd071d880f546ea
eric <744731059@qq.com>
2016-11-03 10:58:46 +0800

    回滚到mengning

--- server/config.js
+++ server/config.js
@@ -1,9 +1,8 @@
 // Warning!!!
 // Do Not Commit this file!!!
-exports.token = "passw0rd";
-exports.appID = "wxe5bbb51184f14bbb";
-exports.appsecret = "5eac6742046a02ccf0dfe330c58c5bd4";
-exports.url = "www.wangdeyi.cc";
-exports.notify_template_id = '6OA_CnK26bNJaEp4zbqouLPBfv9k0dpb6SsCz7iv5X0';
-exports.follow_template_id = '	X5kOCH3gkmlt5dFvgB-TwAqzBBpXN0OgTYctixY7gyQ';
-exports.register_template_id = '0-DB9x3A69-DADqrQip4q8FW73-2r3qmQ0cR66BOyQQ';
+exports.token = 'YOUR_TOKEN';
+exports.appID = 'YOUR_APPID';
+exports.appsecret = 'YOUR_APPSECRET';
+exports.url = 'YOUR_DOMAIN_NAME';
+exports.notify_template_id = 'YOUR_TEMPLET_ID';
+exports.follow_template_id = 'YOUR_TEMPLET_ID';
